896e51d591aca5c1e8cf5aa9eca576a9f01197c9
Himanshu Kabra <himanshukabra2212@gmail.com>
2023-07-21 19:58:34 +0530

    changes

--- src/components/DrawerMain.js
+++ src/components/DrawerMain.js
@@ -2,11 +2,8 @@ import React from 'react'
 import { Box, Divider, Drawer, Toolbar } from '@mui/material';
 import ContributersMain from './ContributersMain';
 import DrawerListing from './DrawerListing';
-// import SearchMain from './SearchMain';
 import SystemInfoBox from './SystemInfoBox';
 import { Outlet } from 'react-router-dom';
-// import Terminal from './system/Terminal';
-// import DockerCLI from "./docker/DockerCLI";
 
 const DrawerMain = () => {
     return (
@@ -25,14 +22,11 @@ const DrawerMain = () => {
             >
                 <Toolbar sx={{ height: "3rem" }} />
                 <SystemInfoBox />
-                {/* <Divider /> */}
-                {/* <SearchMain /> */}
                 <DrawerListing />
                 <Toolbar sx={{ flexGrow: 1 }} />
                 <Divider />
                 <ContributersMain />
             </Drawer>
-
             <Box>
                 <Outlet />
             </Box>
